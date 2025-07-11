05f43cfdacb172915b5279775ee6e517e10fcb2d
falcon-ov <vinifon4he@gmail.com>
2025-07-11 19:33:30 +0300

    feat: add user and admin auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -269,7 +269,7 @@
         </a>
 
         <div class="logo">
-            <a href="index.html">
+            <a href="{{ route('home.index') }}">
                 <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
             </a>
         </div>
@@ -308,7 +308,7 @@
             <div class="overflow-hidden">
                 <ul class="navigation__list list-unstyled position-relative">
                     <li class="navigation__item">
-                        <a href="index.html" class="navigation__link">Home</a>
+                        <a href="{{ route('home.index') }}" class="navigation__link">Home</a>
                     </li>
                     <li class="navigation__item">
                         <a href="shop.html" class="navigation__link">Shop</a>
@@ -389,7 +389,7 @@
     <div class="container">
         <div class="header-desk header-desk_type_1">
             <div class="logo">
-                <a href="index.html">
+                <a href="{{ route('home.index') }}">
                     <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
@@ -397,7 +397,7 @@
             <nav class="navigation">
                 <ul class="navigation__list list-unstyled d-flex">
                     <li class="navigation__item">
-                        <a href="index.html" class="navigation__link">Home</a>
+                        <a href="{{ route('home.index') }}" class="navigation__link">Home</a>
                     </li>
                     <li class="navigation__item">
                         <a href="shop.html" class="navigation__link">Shop</a>
@@ -461,14 +461,26 @@
                     </div>
                 </div>
 
+                @guest
                 <div class="header-tools__item hover-container">
-                    <a href="login.html" class="header-tools__item">
+                    <a href="{{ route('login') }}" class="header-tools__item">
                         <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
                             <use href="#icon_user" />
                         </svg>
                     </a>
                 </div>
+                @else
+                    <div class="header-tools__item hover-container">
+                        <a href="{{ Auth::user()->utype == 'ADM' ? route('admin.index') : route('user.index') }}" class="header-tools__item">
+                            <span class="pf-6px">{{Auth::user()->name}}</span>
+                            <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <use href="#icon_user" />
+                            </svg>
+                        </a>
+                    </div>
+                @endguest
 
                 <a href="wishlist.html" class="header-tools__item">
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -496,12 +508,12 @@
         <div class="row row-cols-lg-5 row-cols-2">
             <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                 <div class="logo">
-                    <a href="index.html">
-                        <img src="{{ asset('assets/images/logo.png') }}" alt="SurfsideMedia" class="logo__image d-block" />
+                    <a href="{{ route('home.index') }}">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="falcon-ov" class="logo__image d-block" />
                     </a>
                 </div>
-                <p class="footer-address">123 Beach Avenue, Surfside City, CA 00000</p>
-                <p class="m-0"><strong class="fw-medium">contact@surfsidemedia.in</strong></p>
+                <p class="footer-address">123 Beach Avenue, falcon-ov City, CA 00000</p>
+                <p class="m-0"><strong class="fw-medium">contact@falcon-ov.in</strong></p>
                 <p><strong class="fw-medium">+1 000-000-0000</strong></p>
 
                 <ul class="social-links list-unstyled d-flex flex-wrap mb-0">
@@ -599,7 +611,7 @@
 
     <div class="footer-bottom">
         <div class="container d-md-flex align-items-center">
-            <span class="footer-copyright me-auto">©2024 Surfside Media</span>
+            <span class="footer-copyright me-auto">©2024 falcon-ov Media</span>
             <div class="footer-settings d-md-flex align-items-center">
                 <a href="privacy-policy.html">Privacy Policy</a> &nbsp;|&nbsp; <a href="terms-conditions.html">Terms &amp;
                     Conditions</a>
@@ -612,7 +624,7 @@
 <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
     <div class="row text-center">
         <div class="col-4">
-            <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="{{ route('home.index') }}" class="footer-mobile__link d-flex flex-column align-items-center">
                 <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_home" />
@@ -622,7 +634,7 @@
         </div>
 
         <div class="col-4">
-            <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="{{ route('home.index') }}" class="footer-mobile__link d-flex flex-column align-items-center">
                 <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_hanger" />
@@ -632,7 +644,7 @@
         </div>
 
         <div class="col-4">
-            <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="{{ route('home.index') }}" class="footer-mobile__link d-flex flex-column align-items-center">
                 <div class="position-relative">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
