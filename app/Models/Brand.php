2693940dd17b5aa1810572743750bddd0101d7dc
Socolov <145919428+falcon-ov@users.noreply.github.com>
2025-07-12 13:25:40 +0300

    feat: add Brand model and migration

---
+++ app/Models/Brand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * @method static \Illuminate\Database\Eloquent\Builder|static orderBy(string $column, string $direction = 'asc')
+ */
+class Brand extends Model
+{
+    //
+}
