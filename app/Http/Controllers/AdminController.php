7d7ef73ea0d3a8090935977e07be4d6380808a91
Socolov <145919428+falcon-ov@users.noreply.github.com>
2025-07-12 14:53:35 +0300

    feat: add admin interface to create new brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
@@ -17,4 +20,35 @@ class AdminController extends Controller
         $brands = Brand::orderBy('id','DESC')->paginate(10);
         return view('admin.brands', compact('brands'));
     }
+
+    public function add_brand()
+    {
+        return view('admin.brand_add');
+    }
+
+    public function brand_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $file_extension = $image->extension();
+            $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+            $destinationPath = public_path('uploads/brands');
+            $image->move($destinationPath, $file_name);
+            $brand->image = $file_name;
+        }
+
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been added successfully!');
+    }
+
 }
